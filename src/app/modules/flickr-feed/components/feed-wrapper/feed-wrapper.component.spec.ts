c4024555d0497ade04c89ab6b869024361b6ffaa
Kosala Yapa <kosalayapa@gmail.com>
2019-08-05 01:32:50 +0800

    Added unit testing for components

--- src/app/modules/flickr-feed/components/feed-wrapper/feed-wrapper.component.spec.ts
+++ src/app/modules/flickr-feed/components/feed-wrapper/feed-wrapper.component.spec.ts
@@ -1,25 +1,46 @@
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { FeedWrapperComponent } from './feed-wrapper.component';
+import { SearchBoxComponent } from '../../../../shared/components/search-box/search-box.component';
 
 describe('FeedWrapperComponent', () => {
-  let component: FeedWrapperComponent;
-  let fixture: ComponentFixture<FeedWrapperComponent>;
+    let component: FeedWrapperComponent;
+    let fixture: ComponentFixture<FeedWrapperComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ FeedWrapperComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [FeedWrapperComponent, SearchBoxComponent],
+            imports: [FormsModule, HttpClientModule]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FeedWrapperComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(FeedWrapperComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should return an array', () => {
+        component.getFeeds({
+            searchString: 'Sri Lanka'
+        });
+        fixture.detectChanges();
+        fixture.whenStable().then(() => {
+            expect(component.feedList).toEqual(jasmine.any(Array));
+        });
+    });
+
+    it('should return an array of objects with title, imageUrl, imageHeight, imageWidth, description properties', () => {
+        component.getFeeds({
+            searchString: 'Sri Lanka'
+        });
+        fixture.detectChanges();
+        fixture.whenStable().then(() => {
+            const keys = Object.keys(component.feedList[0]).sort();
+            const expected = ['title', 'imageUrl', 'imageHeight', 'imageWidth', 'description'].sort();
+            expect(keys).toEqual(expected);
+        });
+    });
 });
--- src/app/shared/components/search-box/search-box.component.spec.ts
+++ src/app/shared/components/search-box/search-box.component.spec.ts
@@ -1,25 +1,56 @@
+import { DebugElement } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { SearchBoxComponent } from './search-box.component';
 
 describe('SearchBoxComponent', () => {
-  let component: SearchBoxComponent;
-  let fixture: ComponentFixture<SearchBoxComponent>;
+    let component: SearchBoxComponent;
+    let fixture: ComponentFixture<SearchBoxComponent>;
+    let debugElement: DebugElement;
+    let inputElement: HTMLElement;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SearchBoxComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [SearchBoxComponent],
+            imports: [FormsModule]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SearchBoxComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SearchBoxComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+        debugElement = fixture.debugElement.query(By.css('input'));
+        inputElement = fixture.nativeElement;
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    });
+
+    it('Initial string should be empty string', () => {
+        expect(component.searchString).toEqual('');
+    });
+    it('should button get called', async(() => {
+        spyOn(component, 'searchClick');
+        const button = fixture.debugElement.nativeElement.querySelector('button');
+        button.click();
+        fixture.whenStable().then(() => {
+            expect(component.searchClick).toHaveBeenCalled();
+        });
+    }));
+    it('should emit event when search button is clicked', async(() => {
+        const button = fixture.debugElement.nativeElement.querySelector('button');
+        spyOn(component.getSearchString, 'emit');
+        button.click();
+        expect(component.getSearchString.emit).toHaveBeenCalled();
+    }));
+    it('should emit correct event object when search button is clicked', async(() => {
+        const button = fixture.debugElement.nativeElement.querySelector('button');
+        component.searchString = 'Sri Lanka';
+        spyOn(component.getSearchString, 'emit');
+        button.click();
+        expect(component.getSearchString.emit).toHaveBeenCalledWith({
+            searchString: 'Sri Lanka'
+        });
+    }));
 });
